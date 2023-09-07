0da576b004ef79ada66195165c35f0231be25466
holdenmg <holdenmgarrison@gmail.com>
2023-09-07 12:27:22 -0400

    Added bootstrap to dependencies in package.json

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import Navigation from './components/Navigation';
+
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
+  return (
+    <>
+      <Navigation />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default App;
