0442d6bb4c0a2da7b23b2d3314933c1d605c6310
holdenmg <holdenmgarrison@gmail.com>
2023-09-22 20:19:54 -0400

    Moved lawnshark to top of portfolio and removed weather dashboard

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,50 +7,42 @@ import weather from '../assets/weather.png'
 import lawn from '../assets/lawn.png'
 
 const projects = [
-{
-  id: 1,
-  name: "Neighborhood Safari",
-  tech: "Handlebars/Node/NoSQL",
-  imgUrl: neighbor,
-  gitUrl:"https://github.com/holdenmg/nieghborhood-safari",
-  link: "https://gentle-ocean-39436-8a803375c53b.herokuapp.com"
-},
-{
-  id: 2,
-  name:"SoundQuest",
-  tech:"HTML/JQuery/CSS/JavaScript",
-  imgUrl: soundquest,
-  gitUrl:"https://github.com/holdenmg/sound-quest",
-  link:"https://holdenmg.github.io/sound-quest/",
-},
-{
-  id: 3,
-  name: "TechBlog",
-  tech: "Node/Handlebars/NoSQL",
-  imgUrl: tech,
-  gitUrl:"https://github.com/holdenmg/tech-blog",
-  link: "https://guarded-tor-22362-c9691c46dfcb.herokuapp.com/"
-},
-{
-  id: 4,
-  name: "Weather Dashboard",
-  tech: "HTML/CSS/JavaScript/OpenWeather",
-  imgUrl: weather,
-  gitUrl:"https://github.com/holdenmg/weather-dashboard",
-  link: "https://holdenmg.github.io/weather-dashboard"
-},
-{id: 5,
-  name: "LawnShark",
-  tech: "MERN Stack",
-  imgUrl: weather,
-  gitUrl:"https://github.com/holdenmg/weather-dashboard",
-  link: "https://holdenmg.github.io/weather-dashboard"}
-
-
+  {
+    id: 1,
+    name: "LawnShark",
+    tech: "MERN Stack",
+    imgUrl: lawn,
+    gitUrl: "https://github.com/Jlausier/LawnShark",
+    link: "https://gentle-wildwood-36558-193385005281.herokuapp.com/"
+  },
+  {
+    id: 2,
+    name: "Neighborhood Safari",
+    tech: "Handlebars/Node/NoSQL",
+    imgUrl: neighbor,
+    gitUrl: "https://github.com/holdenmg/nieghborhood-safari",
+    link: "https://gentle-ocean-39436-8a803375c53b.herokuapp.com"
+  },
+  {
+    id: 3,
+    name: "SoundQuest",
+    tech: "HTML/JQuery/CSS/JavaScript",
+    imgUrl: soundquest,
+    gitUrl: "https://github.com/holdenmg/sound-quest",
+    link: "https://holdenmg.github.io/sound-quest/",
+  },
+  {
+    id: 4,
+    name: "TechBlog",
+    tech: "Node/Handlebars/NoSQL",
+    imgUrl: tech,
+    gitUrl: "https://github.com/holdenmg/tech-blog",
+    link: "https://guarded-tor-22362-c9691c46dfcb.herokuapp.com/"
+  },
 ]
 
 export default function Portfolio() {
-    return (
-      <PortfolioCard projects = {projects} />
-    );
-  }
+  return (
+    <PortfolioCard projects={projects} />
+  );
+}
